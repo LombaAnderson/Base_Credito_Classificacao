92fb9c1e499eeb5830e2529704dab875bdea7a2d
Anderson Lomba <andersonlomba172@gmail.com>
2021-04-12 14:46:25 -0300

    Atualização da análise da base de dados usando matriz de confusão

--- Base_Credito.py
+++ Base_Credito.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+
 """
 Created on Sat Apr 10 22:50:57 2021
 
@@ -53,8 +53,28 @@ previsoes = classificador.predict(previsores_teste)
 
 from sklearn.metrics import confusion_matrix, accuracy_score
 precisao = accuracy_score(classe_teste, previsoes)
+matriz = confusion_matrix(classe_teste, previsoes )
 
 # precisao * 100 : Acerto de 93,8%
+
+''' Análise de matriz (confusion_matrix)
+
+Linha 0 , coluna 0 - Quantos registros o algoritmo classificou como pessoas que não pagaram o empréstimo (428)
+
+Linha 0, coluna 1 - Registros dos clientes que não pagaram,mas que o algoritmo classificou como pagadores(8)
+
+Linha 1, coluna 0 - Registros dos clientes que pagaram ,porém o algoritmo os classificou como não pagadores (23)
+
+Linha 1, coluna 1 - 41 registros foram classificados como pagadores e o algoritmo os classificou como pagadores
+
+Para saber exatamente quantos registros foram classificados corretamente soma -se 428 + 41 = 469
+
+Para saber exatamente quantos registros foram classificados com erro soma-se 23 + 8 = 31
+
+E para conferir se todos os valores analisados batem com o total (500) basta somar todos os valores da tabela matriz
+428 + 8 + 23 + 41 = 500
+
+'''
 
 
 
